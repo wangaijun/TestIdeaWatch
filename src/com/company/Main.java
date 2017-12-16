3b2babab9221bbc62a73a7f6a2bd40915dd8dd00
wangaj <wangaj@glodon.com>
2017-12-16 08:30:51 +0800

    print hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("hello,world");
+        for (int i=0;i<5;i++) {
+            System.out.printf("hello,world");
+        }
     }
 }
