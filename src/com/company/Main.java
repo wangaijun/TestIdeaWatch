86e66b961187920aa2ad8a697aaf5d17daefc1ed
wangaj <wangaj@glodon.com>
2017-12-16 12:28:25 +0800

    type 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
         for (int i=0;i<5;i++) {
             System.out.printf("hello,world");
         }
-        System.out.println(5);
+        System.out.println(6);
     }
 }
